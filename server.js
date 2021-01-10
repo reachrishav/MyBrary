4ee197612235939bce843f2d4870362b5eeb2168
Rishav <reachrishav@gmail.com>
2021-01-10 20:11:13 +0530

    Added Initial Book Routes

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
 }
 
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
-const mongoose = require('mongoose')
-const indexRouter = require('./routes/index')
-const authorRouter = require('./routes/authors')
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
-app.set('view engine','ejs')
-app.set('views',__dirname+'/views')
-app.set('layout','layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded())
-app.use('/', indexRouter)
-app.use('/authors/', authorRouter)
-app.listen(process.env.PORT || 3000)
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
 
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true,useUnifiedTopology: true })
-.then(()=>{
-    console.log(`DB Ok`)
-})
-.catch(err=>{
-    console.log(`db error ${err.message}`);
-    process.exit(-1)
-})
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRouter);
+app.use("/authors/", authorRouter);
+app.use("/books/", bookRouter);
+
+app.listen(process.env.PORT || 3000);
+
+mongoose
+    .connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log(`DB Ok`);
+    })
+    .catch((err) => {
+        console.log(`db error ${err.message}`);
+        process.exit(-1);
+    });
